2b62a903723a94a653a0d14d0e6d68f6c3a9b39e
Robert Slade <rslade97@gmail.com>
2019-02-25 02:44:25 +0000

    Set up DB model

--- app_server/controllers/blog.js
+++ app_server/controllers/blog.js
@@ -1,8 +1,8 @@
 /* Get Blog pages. */
 module.exports.blogadd = function(req, res) {
-  res.render('blogadd', { title: 'Blog Add' });
+  res.render('blogadd', { title: 'Add Blog' });
 };
 
 module.exports.bloglist = function(req, res) {
-  res.render('bloglist', { title: 'Blog List' });
+  res.render('bloglist', { title: 'List Blogs' });
 };
