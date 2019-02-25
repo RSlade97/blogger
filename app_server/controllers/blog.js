fe84d6d4e7ac0f2fd31a935270bc9e709d59b07c
Robert Slade <rslade97@gmail.com>
2019-02-25 04:33:24 +0000

    Set up edit and delete blog pages

--- app_server/controllers/blog.js
+++ app_server/controllers/blog.js
@@ -4,5 +4,29 @@ module.exports.blogadd = function(req, res) {
 };
 
 module.exports.bloglist = function(req, res) {
-  res.render('bloglist', { title: 'List Blogs' });
+  res.render('bloglist', { title: 'List Blogs',
+                           blogs: [{
+                                      blogTitle: 'Welcome',
+                                      blogText: 'Sample Text',
+                                      createdOn: '24 February 2019'
+                                  }, {
+                                      blogTitle: 'To',
+                                      blogText: 'More Sample Text',
+                                      createdOn: '24 February 2219',
+                                  }, {
+                                      blogTitle: 'My Blog!',
+                                      blogText: 'Even More Sample Text',
+                                      createdOn: '24 February 2319'
+                                  }] 
+                         });
+};
+
+module.exports.blogedit = function (req, res) {
+  res.render('blogedit', { title: 'Edit Blog',
+	                   message: 'Under construction' });
+};
+
+module.exports.blogdelete = function (req, res) {
+  res.render('blogdelete', { title: 'Delete Blog',
+                             message: 'Under construction' });
 };
