9323954228e462b85755abad53deebab5b710c0f
Robert Slade <rslade97@gmail.com>
2019-04-15 21:42:00 +0000

    Add user authentication in api

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var ctrlBlogs = require('../controllers/blogs');
+var ctrlAuth = require('../controllers/authentication');
 
 router.get('/blogs', ctrlBlogs.blogsList);
 router.get('/blogs/:id', ctrlBlogs.blogShow);
 router.post('/blogs', ctrlBlogs.blogAdd);
 router.put('/blogs/:id', ctrlBlogs.blogEdit);
 router.delete('/blogs/:id', ctrlBlogs.blogDelete);
-
+router.post('/register', ctrlAuth.register);
+router.post('/login', ctrlAuth.login);
 module.exports = router;
